322c7094154deb968eda2fa6e35c9158c0da6770
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-06-11 12:30:15 -0400

    Define apiRouter
    Co-authored-by: lovelyjoy1991 <lizhijiaocs@gmail.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,13 @@
-import express from "express";
+import express from 'express';
+import apiRouter from './routes/apiRouter.js'; // not defined yet
 
 const app = express();
 
 const PORT = 3000;
+
+app.use('/api', apiRouter);
+
+// define global error handler here
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);

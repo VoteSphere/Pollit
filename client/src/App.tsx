8f9f0833c2840ab0c360d43b6cd3f3e9355f8bf4
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-06-11 11:56:06 -0400

    Initial repo setup and configuration files
    Co-authored-by: lovelyjoy1991 <lizhijiaocs@gmail.com>
    Co-authored-by: nickangel7
    Co-authored-by: ReefHall <shareifhall@yahoo.com>
    Co-authored-by: TrippMurphy <trippiiimurphy@gmail.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/index.css';
+
+const App: React.FC = () => (
+  <Router>
+    <Routes>
+      {/* example route */}
+      {/* <Route path='/' element={<HomePage />} /> */}
+    </Routes>
+  </Router>
+);
+
+export default App;

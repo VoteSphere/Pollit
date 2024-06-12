ea3ce0d1adae7475b33b81c818cae34f93d428c7
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-06-11 20:21:31 -0400

    Complete create poll feature - BE only
    Co-authored-by: lovelyjoy1991 <lovelyjoy1991@users.noreply.github.com>
    Co-authored-by: TrippMurphy <trippiiimurphy@gmail.com>

--- server/src/controllers/pollController.ts
+++ server/src/controllers/pollController.ts
@@ -4,6 +4,9 @@ import type {
   ServerError /* DbPollRequest */,
 } from '../../../types/api.d.ts';
 import { Request, Response, NextFunction } from 'express';
+import { pool } from '../db/connect.js';
+import pg from 'pg';
+import { INSERT_POLL, INSERT_POLL_ITEM } from '../db/queries.js';
 
 // function to generate random string
 function generateRandomString(length: number): string {
@@ -16,16 +19,43 @@ function generateRandomString(length: number): string {
   return result;
 }
 // function to handle create poll feature
-export const createPoll = (req: Request, res: Response, next: NextFunction) => {
+export const createPoll = async (req: Request, res: Response, next: NextFunction) => {
   // generate short link(6-index url)
   const url: string = generateRandomString(6);
+  const urlSlug = `/p/${url}`;
 
-  // const body = req.body as NewPollRequest;
-  // const poll: DbPollRequest = { ...body, url };
-  // query the database (insert)
+  const body = req.body as NewPollRequest;
+  const { description, name, items, maxVotes } = body;
+
+  interface InsertQueryRes extends pg.QueryResult {
+    rows: [
+      {
+        _id: number;
+      },
+    ];
+  }
+
   try {
-    // const result = db.pool(...);
-    const pollInfo: NewPollResponse = { urlSlug: `/p/${url}` };
+    // to do: use a transaction and pipeline/batch requests
+    // insert poll into the polls table and retrieve id
+    const response = (await pool.query(INSERT_POLL, [
+      description,
+      name,
+      urlSlug,
+      maxVotes,
+    ])) as InsertQueryRes;
+    if (response.rowCount !== 1) throw new Error();
+    const pollId = response?.rows[0]?._id;
+
+    // for each poll item, insert into poll_item table
+    const promises: Promise<pg.QueryResult>[] = [];
+    items.forEach((e) => {
+      promises.push(pool.query(INSERT_POLL_ITEM, [e.name, pollId]));
+    });
+
+    await Promise.all(promises);
+
+    const pollInfo: NewPollResponse = { urlSlug };
     res.locals.pollInfo = pollInfo;
     next();
   } catch (err: unknown) {
